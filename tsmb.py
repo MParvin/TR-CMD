9ada05ed30af33b20a1cc4213166892b0f67331c
Mohammad Parvin <m.parvin986@gmail.com>
2018-04-07 00:06:56 +0430

    runCMD, ping8, topCMD and HTopCMD updated

--- tsmb.py
+++ tsmb.py
@@ -10,6 +10,7 @@ Created on Wed Apr  4 19:06:08 2018
 debug = True
 
 import subprocess
+import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
@@ -22,16 +23,21 @@ logger = logging.getLogger(__name__)
 ### This function run command and send output to user
 def runCMD(bot, update):
     usercommand = update.message.text
-    cmdOut = str(subprocess.Popen(usercommand,
-                                  shell=True, stdout=subprocess.PIPE,
-                                  stderr=subprocess.STDOUT))
-    update.message.reply_text(cmdOut)
+    cmdProc = subprocess.Popen(usercommand,
+                                         stdout=subprocess.PIPE, 
+                                         stderr=subprocess.PIPE,
+                                         shell=True)
+    cmdOut, cmdErr = cmdProc.communicate()
+    if cmdOut:
+        update.message.reply_text(str(cmdOut,'utf-8'))
+    else:
+        update.message.reply_text(str(cmdErr,'utf-8'))
     
 ### This function ping 8.8.8.8 and send you result    
 def ping8(bot, update):
-    cmdOut = str(subprocess.Popen('ping 8.8.8.8 -c2',
-                                  shell=True, stdout=subprocess.PIPE,
-                                  stderr=subprocess.STDOUT))
+    cmdOut = str(subprocess.check_output('ping' , '8.8.8.8 -c4',
+                                         stderr=subprocess.STDOUT,
+                                         shell=True),'utf-8')
     update.message.reply_text(cmdOut)
     
 def startCMD(bot, update):
@@ -39,6 +45,27 @@ def startCMD(bot, update):
 
 def helpCMD(bot, update):
     update.message.reply_text("This bot has access to your server/PC, So it can do anything. Please use Telegram local password to prevent others from accessing to this bot.")    
+
+def topCMD(bot, update):
+    cmdOut = str(subprocess.check_output('top -n 1',
+                                  shell=True),'utf-8')
+    update.message.reply_text(cmdOut)
+
+def HTopCMD(bot, update):
+    htopCheck = subprocess.call(['which','htop'])
+    if htopCheck != 0:
+        update.message.reply_text("htop is not installed on your system, Please install it first and try again")
+        return
+    ahaCheck = subprocess.call(['which','aha'])
+    if ahaCheck != 0:
+        update.message.reply_text("aha is not installed on your system, Please install it first and try again")
+        return
+    chat_id = update.message.chat_id
+    os.system('echo q | htop | aha --black --line-fix  > ./htop-output.html')
+    with open('./htop-output.html',"rb") as fileToSend:
+        bot.sendDocument(document=fileToSend,chat_id=chat_id)
+    if os.path.exists('./htop-output.html'):
+        os.remove('./htop-output.html')
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
@@ -50,13 +77,13 @@ def main():
     
     dp.add_handler(CommandHandler("start", startCMD))
     dp.add_handler(CommandHandler("ping8", ping8))
+    dp.add_handler(CommandHandler("top", topCMD))
+    dp.add_handler(CommandHandler("htop", HTopCMD))
     dp.add_handler(CommandHandler("help", helpCMD))
     dp.add_handler(MessageHandler(Filters.text, runCMD))
     
     dp.add_error_handler(error)
-    
     updater.start_polling()
-    
     updater.idle()
     
 if __name__ == '__main__':
