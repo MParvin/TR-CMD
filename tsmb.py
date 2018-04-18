1608a659feb4f8e904c70e213688419bdc29dfe8
Mohammad Parvin <m.parvin986@gmail.com>
2018-04-18 13:09:21 +0430

    isAdmin updated

--- tsmb.py
+++ tsmb.py
@@ -91,10 +91,12 @@ def error(bot, update, error):
  
 def isAdmin(bot, update):
     chat_id = update.message.chat_id
-    if chat_id == adminCID:
+    if str(chat_id) == adminCID:
         return True
     else:
         update.message.reply_text("You cannot use this bot, because you are not Admin!!!!")
+        alertMessage = """Some one try to use this bot with this information:\n chat_id is {} and username is {} """.format(update.message.chat_id,update.message.from_user.username)
+        bot.sendMessage(text=alertMessage, chat_id=adminCID)
         return False
     
 def main():
