f9e160857de5b6222000c8f18e303f03b8aa0f5d
Mohammad Parvin <m.parvin986@gmail.com>
2021-07-19 00:45:02 +0430

    logger module detached from bot.py

---
+++ modules/logger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+### Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
