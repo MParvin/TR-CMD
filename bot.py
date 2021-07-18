71642bf6b64cb0e0a22f1b4c56ae8130a97de5f9
Mohammad Parvin <m.parvin986@gmail.com>
2021-07-19 00:50:42 +0430

    monitoring module detached from bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ import modules.logger
 from modules.check_admin import isAdmin
 from modules.run_commands import runCMD
 from modules.run_ping import ping8
+from modules.monitoring_commands import *
 
 
 def startCMD(bot, update):
@@ -43,42 +44,6 @@ def helpCMD(bot, update):
         "This bot has access to your server/PC, So it can do anything. Please use Telegram local password to prevent others from accessing to this bot.",
         chat_id=adminCID,
     )
-
-
-def topCMD(bot, update):
-    if not isAdmin(bot, update):
-        return
-    cmdOut = str(subprocess.check_output("top -n 1", shell=True), "utf-8")
-    bot.sendMessage(text=cmdOut, chat_id=adminCID)
-    bot.sendMessage(text=cmdOut, chat_id=adminCID)
-
-
-def HTopCMD(bot, update):
-    ## Is this user admin?
-    if not isAdmin(bot, update):
-        return
-    ## Checking requirements on your system
-    htopCheck = subprocess.call(["which", "htop"])
-    if htopCheck != 0:
-        bot.sendMessage(
-            text=
-            "htop is not installed on your system, Please install it first and try again",
-            chat_id=adminCID,
-        )
-        return
-    ahaCheck = subprocess.call(["which", "aha"])
-    if ahaCheck != 0:
-        bot.sendMessage(
-            text=
-            "aha is not installed on your system, Please install it first and try again",
-            chat_id=adminCID,
-        )
-        return
-    os.system("echo q | htop | aha --black --line-fix  > ./htop-output.html")
-    with open("./htop-output.html", "rb") as fileToSend:
-        bot.sendDocument(document=fileToSend, chat_id=adminCID)
-    if os.path.exists("./htop-output.html"):
-        os.remove("./htop-output.html")
 
 
 def error(bot, update, error):
