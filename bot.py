949c0d5ebeeba9f5005bc993cb42d4a3862d69fe
Mohammad Parvin <m.parvin@fidibo.com>
2022-02-05 16:15:04 +0330

    remove extra else

--- bot.py
+++ bot.py
@@ -114,15 +114,15 @@ def isAdmin(bot, update):
     chat_id = update.message.chat_id
     if str(chat_id) == adminCID:
         return True
-    else:
-        update.message.reply_text(
-            "You cannot use this bot, because you are not Admin!!!!"
-        )
-        alertMessage = """Some one try to use this bot with this information:\n chat_id is {} and username is {} """.format(
-            update.message.chat_id, update.message.from_user.username
-        )
-        bot.sendMessage(text=alertMessage, chat_id=adminCID)
-        return False
+    
+    update.message.reply_text(
+        "You cannot use this bot, because you are not Admin!!!!"
+    )
+    alertMessage = """Some one try to use this bot with this information:\n chat_id is {} and username is {} """.format(
+        update.message.chat_id, update.message.from_user.username
+    )
+    bot.sendMessage(text=alertMessage, chat_id=adminCID)
+    return False
 
 
 def main():
